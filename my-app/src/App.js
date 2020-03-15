1c890f69b09dba767ba7cb530cd6efcee7b84181
Naeem Mannan <nmannan97@gmail.com>
2020-03-15 13:53:02 -0700

    adding more files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> My token = {window.token}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
