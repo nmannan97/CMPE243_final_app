4a322aa5b6c252e6c602f9881f9aa810434e6238
Naeem Mannan <nmannan97@gmail.com>
2020-03-16 16:16:39 -0700

    adding changes

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -5,12 +5,12 @@ app = flask.Flask("__main__")
 @app.route('/')
 def my_index():
     return flask.render_template('index.html', token = 'hello world')
-
+'''
 @app.route('/test', methods = ['GET'])
 def test():
     if flask.request.args.get('lat', None):
         return "success"
     else:
         return "Fail"
-
+'''
 app.run(debug = True)
