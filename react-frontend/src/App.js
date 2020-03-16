fb7b65b3284f555bb4e1308d18c5e1cefab2744d
Naeem Mannan <nmannan97@gmail.com>
2020-03-16 15:40:25 -0700

    adding for backup

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-
-     
+      
         <a
           className="App-link"
           href="https://reactjs.org"
